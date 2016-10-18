f12d563b7cd9057e1ee92d86c7dca882fa08d9d7
Georgi Stoimenov <Gesh4o@users.noreply.github.com>
2016-10-18 09:23:19 +0300

    Update 07. ReplaceATag.cs

--- Programming Fundamentals/07. StringsAndRegEx/07. ReplaceATag.cs
+++ Programming Fundamentals/07. StringsAndRegEx/07. ReplaceATag.cs
@@ -1,7 +1,7 @@
 namespace StringsAndRegex
 {
-	using System;
-	using System.Text.RegularExpressions;
+    using System;
+    using System.Text.RegularExpressions;
 
     class ReplaceTagMain
     {
@@ -13,10 +13,10 @@ namespace StringsAndRegex
                 string pattern = @"<a.*href=((?:.|\n)*?(?=>))>((?:.|\n)*?(?=<))<\/a>";
                 string replace = @"[URL href=$1]$2[/URL]";
                 
-				string replaced = Regex.Replace(text, pattern, replace);
+		string replaced = Regex.Replace(text, pattern, replace);
                 Console.WriteLine(replaced);
                 
-				text = Console.ReadLine();
+		text = Console.ReadLine();
             }
         }
     }
